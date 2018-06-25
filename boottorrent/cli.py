fc89bc49a9e425f162210c9b960c1fdb9bb87f34
Shreyansh Khajanchi <shreyansh_k@live.com>
2018-06-25 13:27:53 +0530

    cli: add code to verify the configuration

--- boottorrent/cli.py
+++ boottorrent/cli.py
@@ -39,6 +39,9 @@ def start():
             exit()
         with open(cfgfilepath, 'r') as cfgfile:
             cfg = yaml.load(cfgfile)
+            valid = verify_config_v0(cfg)
+            if not valid:
+                exit()
         bt = BootTorrent(cfg, wd)
         bt.start()
     else:
@@ -49,20 +52,177 @@ def start():
 
 
 @click.command()
-def stop(args=None):
-    """Shutdown the running processes."""
-    click.echo("I'll shutdown the services.")
-
-
-@click.command()
 def version(args=None):
     """Display the version number"""
     click.echo(f"BootTorrent {__version__}")
 
 
+def verify_config_v0(cfg):
+    """Function to validate the correctness
+    of the configuration
+    """
+    # 0. Assert existence
+    # 1. Assert type
+    # 2. Assert value
+    if cfg is None:
+        err("configuration not found.","make sure you're in correct directory.")
+    if type(cfg) is not dict:
+        err("invalid configuration.", "check configuration.")
+    sections = ['aria2', 'boottorrent', 'dnsmasq', 'opentracker','transmission']
+    for section in sections:
+        s = cfg.get(section, None)
+        if not s:
+            err(f"Section '{section}' not found.",
+                "check your configuration.")
+        if type(s) is not dict:
+            err(f"invalid value for '{section}'.",
+                "check it's value.")
+    # check boottorrent section
+    bt = cfg['boottorrent']
+    if bt.get('version', "") != 0:
+        err("Unsupported version.",
+            "use correct version of BootTorrent.")
+    do = bt.get('display_oss', "")
+    if len(do) == 0:
+        err("No OSs are enabled.",
+            "add an OS to display_oss parameter.")
+    if type(do) is not list:
+        err("display_oss is not a list.",
+            "correct parameter value.")
+    else:
+        for val in do:
+            if type(val) is not str:
+                err("Invalid value in display_oss.",
+                     "make sure it's a string.")
+    try:
+        to = int(bt['timeout'])
+    except KeyError:
+        err("timeout not found.", "add timeout value.")
+    except ValueError:
+        err("Invalid value for timeout.", "correct it's value.")
+    df = bt.get("default_os", "")
+    if len(df) == 0:
+        err("default_os not set.", "set correct value.")
+    if type(df) is not str:
+        err("incorrect value for default_os", "check it's value.")
+    if df not in do:
+        err(f"{df} not found in display_oss.", "check it's value.")
+    try:
+        st = int(bt['seed_time'])
+        if st < 0:
+            err("Negative value for seed_time",
+                "correct it's value.")
+    except KeyError:
+        err("seed_time not found.", "set it's value.")
+    except ValueError:
+        err("Invalid value for seed_time.", "correct it's value.")
+    hi = bt.get("host_ip", "")
+    if len(hi) == 0:
+        err("host IP not set.", "set host_ip field.")
+    ip = hi.split('.')
+    if len(ip) != 4:
+        err("invalid host IP.", "correct it's value.")
+    for i in ip:
+        try:
+            j = int(i)
+            if not 0 <= j <= 255:
+                err("Invalid host IP.", "correct it's value.")
+        except ValueError:
+            err("Invalid host IP.", "correct it's value.")
+    # check dnsmasq section
+    dm = cfg['dnsmasq']
+    dhcp = dm.get("enable_dhcp", "")
+    if not dhcp:
+        inf("DHCP server is disabled.")
+    if type(dhcp) is not bool:
+        err("Invalid value for dnsmasq.enable_dhcp",
+            "set it to a bool value.")
+    intf = dm.get("interface", "")
+    if not intf:
+        err("You've not set the interface.",
+            "configure a network interface.")
+    dr = dm.get('dhcp_range', "")
+    if not dr:
+        err("You've not set an IP range for DHCP server."
+            "configure it's value.")
+    et = dm.get("enable_tftp", "")
+    if not et:
+        inf("TFTP server is disabled.")
+    if type(et) is not bool:
+        err("Invalid value for dnsmasq.enable_tftp",
+            "set it to a bool value.")
+    # check opentracker section
+    ot = cfg['opentracker']
+    ote = ot.get("enable", "")
+    if not ote:
+        inf("Internal Tracker disabled.", "Consider enabling it.")
+    if type(ote) is not bool:
+        err("Invalid value for opentracker.enable",
+            "set it to a bool value.")
+    otp = ot.get("port", "")
+    if ote and not otp:
+        err("Tracker port not configured.", "configure it's value")
+    try:
+        otp = int(otp)
+        if not 0 < otp < 65536:
+            err("Invalid value for Opentracker port.",
+                "correct it's value.")
+    except ValueError:
+        err("Invalid value for Opentracker port.", "correct it's value.")
+    # check transmission section
+    tr = cfg['transmission']
+    trp = tr.get("rpc_port", "")
+    if not trp:
+        err("Transmission port not configured.", "configure it's value")
+    try:
+        trp = int(trp)
+        if not 0 < trp < 65536:
+            err("Invalid value for Transmission port.",
+                "correct it's value.")
+    except ValueError:
+        err("Invalid value for Transmission port.", "correct it's value.")
+    tre = tr.get("lpd_enabled", "")
+    if tre and type(tre) is not bool:
+        err("Invalid value for transmission.lpd_enabled",
+            "set it to a bool value.")
+    # check aria2 section
+    a2 = cfg['aria2']
+    a2lpd = a2.get("bt_enable_lpd", "")
+    if type(a2lpd) is not bool:
+        err("Invalid value for aria2.bt_enable_lpd",
+            "set it to a bool value.")
+    if not ote and (not tre or not a2lpd):
+        err("You've not enabled any peer discovery mechanism.",
+            "Please either enable LPD or Opentracker.")
+    elif not ote and tre and a2lpd:
+        inf("You've enabled LPD but it is slow and unreliable.",
+            "consider enabling Opentracker.")
+    # all checks passed
+    return True
+
+
+def err(err, action):
+    click.echo("Error! " + err)
+    click.echo("Please " + action)
+    exit()
+
+def inf(info, rec=None):
+    click.echo("Information: " + info)
+    if rec:
+        click.echo("Recommendation: " + rec)
+    click.echo("Continue? (y/n)")
+    i = input()
+    if i == 'y' or i == 'Y':
+        return
+    elif i == 'n' or i == 'N':
+        exit()
+    else:
+        click.echo("Error: Invalid input. Aborting!")
+        exit()
+
+
 main.add_command(init)
 main.add_command(start)
-main.add_command(stop)
 main.add_command(version)
 
 
